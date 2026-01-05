573b822bab49fae43abf9d3e499734d698456333
Anurup R Krishnan <anuruprkrishnan@gmail.com>
2026-01-05 21:01:53 +0530

    Upload SecureMed API project from IntelliJ

---
+++ src/main/java/com/securemed/securemedapi/SecuremedApiApplication.java
@@ -0,0 +1,13 @@
+package com.securemed.securemedapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuremedApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecuremedApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/securemed/securemedapi/SecuremedApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securemed.securemedapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuremedApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
