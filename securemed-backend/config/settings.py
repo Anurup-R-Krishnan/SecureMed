b7591c07ddc0b84bd9ce34c8864ebbb3cf6078d5
Varun Raj V <168104530+VarunRajV28@users.noreply.github.com>
2026-03-11 09:42:43 +0530

    Update default permission class to AllowAny
    Changed default permission class from IsAuthenticated to AllowAny.

--- securemed-backend/config/settings.py
+++ securemed-backend/config/settings.py
@@ -194,7 +194,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',  # Change this from IsAuthenticated
     ],
     'DEFAULT_PAGINATION_CLASS': 'apps.platform.core.pagination.StandardResultsSetPagination',
     'PAGE_SIZE': 10,
